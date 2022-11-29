29a115490a421f0175a3a649c2c930f24b3aa140
mStirner <marc.stirner@hotmail.de>
2022-11-29 19:42:42 +0100

    Tile style changed

--- src/views/Device.vue
+++ src/views/Device.vue
@@ -66,33 +66,39 @@ export default {
 
 <template>
   <div class="container-fluid">
-    <!-- DONT GROUP -->
-    <div class="row">
-      <RouterLink
-        v-bind:key="item._id"
-        v-for="item in endpoints"
-        custom
-        :to="{
-          name: '/endpoints/:_id',
-          params: {
-            _id: item._id,
-          },
-        }"
-        v-slot="{ href, navigate }"
-      >
-        <div class="col-2 mb-4">
-          <Tile :href="href" @click="navigate" class="bg-dark border-secondary">
-            <template #icon>
-              <i class="fa-2xl" :class="item.icon"></i>
-            </template>
-            <template #title>{{ item.name }} </template>
-            <span class="text-secondary fw-light">
-              ({{ getRoomNameById(item.room) }})
-            </span>
-          </Tile>
-        </div>
-      </RouterLink>
+    <div class="h-100">
+      <!-- DONT GROUP -->
+      <div class="row h-100 display-flex text-center">
+        <RouterLink
+          v-bind:key="item._id"
+          v-for="item in endpoints"
+          custom
+          :to="{
+            name: '/endpoints/:_id',
+            params: {
+              _id: item._id,
+            },
+          }"
+          v-slot="{ href, navigate }"
+        >
+          <div class="p-0 col-6 col-md-3 col-xl-2">
+            <Tile
+              :href="href"
+              @click="navigate"
+              style="background: transparent; border: 1px solid #000"
+            >
+              <template #icon>
+                <i class="fa-2xl" :class="item.icon"></i>
+              </template>
+              <template #title>{{ item.name }} </template>
+              <span class="text-secondary fw-light">
+                ({{ getRoomNameById(item.room) }})
+              </span>
+            </Tile>
+          </div>
+        </RouterLink>
+      </div>
+      <!-- DONT GROUP -->
     </div>
-    <!-- DONT GROUP -->
   </div>
 </template>
