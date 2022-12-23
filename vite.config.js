0dae68e1c264ea03c1e9e00ffd9e58888804235b
mStirner <marc.stirner@hotmail.de>
2022-12-23 20:24:03 +0100

    fix #21

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,9 @@ export default defineConfig({
             "/api": {
                 target: "http://127.0.0.1:8080",
                 ws: true
+            },
+            "/auth": {
+                target: "http://127.0.0.1:8080",
             }
         }
     },
