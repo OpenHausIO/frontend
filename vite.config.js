abe47521388c6fc20a8585ead5493604c9b48a6b
mStirner <marc.stirner@hotmail.de>
2023-01-06 13:08:48 +0100

    env variables for reverse proxy added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const {
+    BACKEND_HOST,
+    BACKEND_PORT,
+    BACKEND_PROTOCOL
+} = process.env = Object.assign({
+    BACKEND_HOST: "127.0.0.1",
+    BACKEND_PORT: "8080",
+    BACKEND_PROTOCOL: "http"
+}, process.env);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,11 +26,11 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://127.0.0.1:8080",
+                target: `${BACKEND_PROTOCOL}://${BACKEND_HOST}:${BACKEND_PORT}`,
                 ws: true
             },
             "/auth": {
-                target: "http://127.0.0.1:8080",
+                target: `${BACKEND_PROTOCOL}://${BACKEND_HOST}:${BACKEND_PORT}`,
             }
         }
     },
