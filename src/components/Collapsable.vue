9b7bddfbaa1fa34607e43c344ff04cadd21166bb
mStirner <marc.stirner@hotmail.de>
2023-01-11 17:39:37 +0100

    fix #60

--- src/components/Collapsable.vue
+++ src/components/Collapsable.vue
@@ -23,20 +23,20 @@ export default {
       style="border-top: 1px solid #000; border-bottom: 1px solid #000"
     >
       <!-- LEFT -->
-      <div class="col">
+      <div class="col-9 col-sm-10">
         <div class="d-inline-flex">
           <slot name="title" id="title"></slot>
         </div>
       </div>
       <!-- LEFT -->
       <!-- RIGHT -->
-      <div class="col-2">
+      <div class="col-3 col-sm-2">
         <div @click="collapsed = !collapsed">
           <div v-if="!collapsed" class="text-center">
             <i class="fa-solid fa-chevron-up fa-2x d-block"></i>
             Collapse
           </div>
-          <div v-else class="text-center">
+          <div v-else class="text-center word-">
             <i class="fa-solid fa-chevron-down fa-2x d-block"></i>
             Expand
           </div>
